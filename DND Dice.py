7ab6567857c8eda3cacfc04c4d74354f6df47bea
NicCapule <78493066+NicCapule@users.noreply.github.com>
2025-05-19 20:35:17 +0800

    Added Log window and logs

--- DND Dice.py
+++ DND Dice.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 from tkinter import * 
 from tkinter import ttk
 
@@ -12,6 +13,7 @@ dice_options = [
 ]
 
 root = Tk()
+root.resizable(False, False)
 root.title("DND Dice Machine")
 
 mainframe = ttk.Frame(root, padding=" 3 3 12 12")
@@ -39,14 +41,23 @@ dice_choice.trace_add("write", update_button_label)
 rolled = StringVar()
 def roll(*args):
    die = choice_conversion()
-   sides = die
-   rolled.set(random.randint(1,sides))
+   result = random.randint(1,die)
+   rolled.set(result)
+   add_log(die, result)
 
 # Convert dice_choice to integer and remove "D"
 def choice_conversion():
     choice = dice_choice.get().replace("D","")
     int_choice = int(choice)
     return int_choice
+
+current_time = datetime.datetime.now()
+remove_microsecond = current_time.replace(microsecond = 0) 
+def add_log(die, result):
+    logWindow.config(state='normal')
+    logWindow.insert(END, f"{remove_microsecond}: Rolled a D{choice_conversion()}: {result}\n")
+    logWindow.see(END)  # Auto-scroll
+    logWindow.config(state='disabled')
 
 ttk.Button(mainframe, textvariable=button_label, command=roll).grid(column=2, row=3, sticky=W)
 
@@ -55,6 +66,9 @@ ttk.Label(mainframe, textvariable=rolled).grid(column=2, row=2, sticky=(W, E))
 ttk.Label(mainframe, text="Choose Dice: ").grid(column=1, row=1, sticky=W)
 ttk.Label(mainframe, text="Result: ").grid(column=1, row=2, sticky=W)
 
+ttk.Label(mainframe, text="Log Window: ").grid(column=3, row=1, sticky=W)
+logWindow = Text(mainframe, height =8, width = 50, state='disabled')
+logWindow.grid(column=3, row = 2, rowspan = 3, sticky = W)
 
 for child in mainframe.winfo_children():
    child.grid_configure(padx = 5, pady = 5)
